701004244d3d908060e290cdfdeb056b3ceda9b6
Connor <connor.ross132@gmail.com>
2023-10-25 21:05:08 +1000

    fixing of bash script. Adding the install of requirements.txt

--- wordle_script.sh
+++ wordle_script.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-if ! [[ -x "$(command -v python3)"]]
-then
-    echo "ERROR: Python 3 is not installed. This is a requirement for the application. For a guide to install Python 3 , please see the README file. Alternatively, visit this link https://realpython.com/installing-python/ >&2
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed. This is a requirement for the application. For a guide to install Python 3 please visit the following link https://realpython.com/installing-python/"
     exit 1
 fi
 
-if ! [[ -x "$(command -v pip3)" ]]
-then
-    echo "ERROR: Pip 3 is not installed. This is a requirement for the application. For a guide to install Pip 3, please see the README file. Alternatively, visit this link https://pypi.org/project/pip/ >&2
+if ! command -v pip3 &> /dev/null; then
+    echo "Pip 3 is not installed. This is a requirement for the application. For a guide to install Pip 3, please see the README file. Alternatively, visit this link https://pypi.org/project/pip/"
 exit 1
 fi
 
+python3 -m pip install --upgrade pip
 
-pip3 install -r requirements.txt 
+python3 -m pip install -r requirements.txt
 python3 main.py
