9236505da19e30fb8f9dabd21516d56714befcb0
Connor <connor.ross132@gmail.com>
2023-10-25 12:34:57 +1000

    Initial Commit

--- main.py
+++ main.py
@@ -2,49 +2,59 @@ import random
 from nltk.corpus import words
 import string
 from colorama import init, Fore
-
+            
+    
 # Initialize colorama
 init(autoreset=True)
 def load_words_from_file(filename):
     with open(filename, 'r') as f:
-# Reading the file, splitting by commas or newlines, and convertting into a set
-        words = set(word.strip() for word in f.read().split(","))
+        word_content = f.read().split("\n")
+        words = {word.strip().lower() for word in word_content}
     return words
 
-valid_words = load_words_from_file('word_list.txt')
+valid_words = load_words_from_file("word_list.txt")
+print(f"Loaded {len(valid_words)} valid words.")
+
 
 def get_feedback(secret_word, guess):
-    feedback = ['R'] * len(secret_word)
-    unused_indices = list(range(len(secret_word)))
+    feedback = []
 
-    # Check for green feedback (correct letter, correct position)
-    for i, (s, g) in enumerate(zip(secret_word, guess)):
-        if s == g:
-            feedback[i] = 'G'
-            unused_indices.remove(i)
-
-    # Check for yellow feedback (correct letter, wrong position)
     for i, g in enumerate(guess):
-        if feedback[i] == 'R' and g in secret_word:
-            for ui in unused_indices:
-                if secret_word[ui] == g:
-                    feedback[i] = 'Y'
-                    unused_indices.remove(ui)
-                    break
+        if g == secret_word[i]:
+            feedback.append('G')
+        elif g in secret_word:
+            feedback.append('Y')
+        else: feedback.append('R')
                     
     return feedback
 
+def choose_game_difficulty():
+    while True:
+        choice = input("Would you like to play (regular) or (hard) mode?").lower()
+        if choice in ["regular", "hard"]:
+            return choice
+        print("Invalid choice. Select either 'regular' or 'hard'.")
+            
+def word_list_based_on_mode(mode):
+    if mode == 'regular':
+        return [word.lower() for word in valid_words if len(word) == 5]
+    elif mode == 'hard':
+        return [word.lower() for word in valid_words if len(word) == 7]
+
 def play_wordle():
-    # Get a list of 5-letter words
-    five_letter_words = [word for word in words.words() if len(word) == 5 and word[0] not in string.ascii_uppercase]
-    secret_word = random.choice(five_letter_words)
+    #defining the wordle mode and picking a random word from the list matching the criteria
+    mode = choose_game_difficulty()
+    word_list = word_list_based_on_mode(mode)
+    print(len(word_list))
     
-    attempts = 6
+    secret_word = random.choice(word_list)
+    
+    attempts = 6 if mode == 'regular' else 5
     while attempts > 0:
-        guess = input(f"Attempt {7 - attempts}: Guess a 5-letter word: ").lower()
+        guess = input(f"Attempt {7 if mode == 'regular' else 6 - attempts}: Guess a {len(secret_word)}-letter word: ").lower()
         
-        if len(guess) != 5:
-            print("Please enter a 5-letter word.")
+        if len(guess) != len(secret_word) or ' ' in guess:
+            print(f"Please enter a {len(secret_word)}-letter word.")
             continue
         elif guess not in valid_words:
             print("Please enter a real word.")
@@ -71,5 +81,4 @@ def play_wordle():
 
     if attempts == 0:
         print(Fore.RED + f"You've run out of attempts. The secret word was: {secret_word}")
-
 play_wordle()
--- making_words.py
+++ making_words.py
@@ -3,17 +3,36 @@ from nltk.corpus import words
 
 UPPERCASE = list(string.ascii_uppercase)
 
-word_list = words.words()
-op = set()
+nltk_word_list = words.words()
+filter_words= [word for word in nltk_word_list if len(word) in [5, 7]]
 
+with open("word_list.txt", "w") as file:
+    for word in filter_words:
+        file.write(f" {word}\n")
 # Writing every 5 letter word listed in the nltk module
-with open("word_list.txt", "w") as word_file:
-    count = 0
-    for word in word_list:
-        if len(word) == 5 and word[0] not in UPPERCASE:
-            if word not in op:
-                op.add(word)
-                count += 1
-                word_file.write(word + ", ")
-                if count % 10 == 0:
-                    word_file.write("\n")
+# with open("word_list.txt", "w") as word_file:
+#     count = 0
+#     for word in word_list:
+#         if len(word) == 5 and word[0] not in UPPERCASE:
+#             if word not in op:
+#                 op.add(word)
+#                 count += 1
+#                 word_file.write(word + ", ")
+#                 if count % 10 == 0:
+#                     word_file.write("\n")
+
+
+#Creating a secondary script for the hard mode to generate every 5 and 7 letter words
+
+# with open("word_list.txt", "w") as word_file:
+#     count = 0
+#     for word in word_list:
+#         if len(word) == 5 and 7 and word[0] not in UPPERCASE:
+#             if word not in op:
+#                 op.add(word)
+#                 count += 1
+#                 word_file.write(word + ", ")
+#                 if count % 10 == 0:
+#                     word_file.write("\n")
+
+
