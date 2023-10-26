1249db7562d3ae4e87e56081607cf2b12980f1e6
Connor <connor.ross132@gmail.com>
2023-10-26 10:31:03 +1000

    adding capability to detect OS for an auto start feature

--- wordle_script.sh
+++ wordle_script.sh
@@ -14,3 +14,22 @@ python3 -m pip install --upgrade pip
 
 python3 -m pip install -r requirements.txt
 python3 main.py
+
+
+# Detecting OS for auto running
+OS="$(uname)"
+
+Case $OS in
+    "Linux")
+        if command -v gnome-terminal &> /dev/null; then
+            gnome-terminal -- python3 ./main.py
+        elif command -v xterm &> /dev/null; then
+            xterm -e python3 ./main.py
+        else 
+            echo "Unknown terminal. Please run the game manually by typing 'Python3 main.py' in the terminal.'"
+        fi
+        ;;
+
+    "Darwin")
+    osascript -e 'tell app "Terminal" to do script "python3 ./main.py"'
+    ;;
